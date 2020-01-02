fef1c0548f3d7af65238e1ac6eea53efd0d9f0b5
stevekutz <stkutz@gmail.com>
2020-01-02 10:41:47 -0600

    changed SpellInput useState to resolve reinspect isssue with names

--- app/src/SpellInput.js
+++ app/src/SpellInput.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {useState} from 'reinspect';
+// import {useState} from 'reinspect';
 import firebase from './firebase';
 
 export const SpellInput = ({spell}) => {
-    const [name, setName] = useState(spell.name, 'SpellInput State');
+    // const [name, setName] = useState(spell.name, 'SpellInput State');
+
+    const [name, setName] = React.useState(spell.name);
 
     const onUpdate = () => {
-        const db = firebase.firestore()
-        db.collection('spells').doc(spell.id).set({...spell, name})
-      }
-    
-      const onDelete = () => {
-        const db = firebase.firestore()
-        db.collection('spells').doc(spell.id).delete()
-      }
+      const db = firebase.firestore()
+      db.collection('spells').doc(spell.id).set({...spell, name})
+    }
+  
+    const onDelete = () => {
+      const db = firebase.firestore()
+      db.collection('spells').doc(spell.id).delete()
+    }
 
     return (
-        <React.Fragment>
+        <div>
         <input
             value={name}
             onChange={e => {
@@ -25,6 +27,6 @@ export const SpellInput = ({spell}) => {
         />
             <button onClick = {onUpdate}> Update </button>
             <button onClick = {onDelete}> Delete </button>
-        </React.Fragment>
+        </div>
     )
 };
